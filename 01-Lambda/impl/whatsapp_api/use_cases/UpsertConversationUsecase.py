3ed7a20e4174bec3b874dd665e7d3f5a82272265
RaphaelRegis <raph4life17@gmail.com>
2026-06-11 21:13:19 -0300

    chore: definição do fluxo de upsert das conversas

--- 01-Lambda/impl/whatsapp_api/use_cases/UpsertConversationUsecase.py
+++ 01-Lambda/impl/whatsapp_api/use_cases/UpsertConversationUsecase.py
@@ -1,5 +1,13 @@
+
+
 def upsert_conversation_usecase(message_data: dict):
 
     print("Criando ou buscando os dados da conversa...")
+    # buscar a conversa
 
+    # achou alguem?
+    # NÃO: cria todos os dados de acordo e salva lá
+    # SIM: continua
+
+    # retorna a conversa
     return {}
