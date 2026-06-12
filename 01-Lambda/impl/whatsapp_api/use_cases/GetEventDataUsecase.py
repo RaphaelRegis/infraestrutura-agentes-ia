65166bef33df2daedf40ea310f4be34296defb4d
RaphaelRegis <raph4life17@gmail.com>
2026-06-11 21:12:13 -0300

    feat: pega o id do numero do agente

--- 01-Lambda/impl/whatsapp_api/use_cases/GetEventDataUsecase.py
+++ 01-Lambda/impl/whatsapp_api/use_cases/GetEventDataUsecase.py
@@ -3,6 +3,7 @@ def get_message_data(event: dict) -> dict:
     print("Pegando dados da mensagem...")
 
     data = {
+        "agent_id": event["entry"][0]["changes"][0]["value"]["metadata"]["phone_number_id"],
         "contactNumber": event["entry"][0]["changes"][0]["value"]["contacts"][0]["wa_id"],
         "contactName": event["entry"][0]["changes"][0]["value"]["contacts"][0]["profile"]["name"]
     }
