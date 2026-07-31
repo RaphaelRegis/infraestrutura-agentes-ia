a15e3fb7a9f63ea2809bb080446b749b1d128904
RaphaelRegis <raph4life17@gmail.com>
2026-07-31 15:31:11 -0300

    feat: infra básica do message adapter para Telegram

---
+++ 02-Message_Adapter/MessageAdapter/src/main/java/com/messageAdapter/MessageAdapter/services/telegram/TelegramAdapter.java
@@ -0,0 +1,11 @@
+package com.messageAdapter.MessageAdapter.services.telegram;
+
+public interface TelegramAdapter {
+
+    public String adaptTextMessage();
+    public String adaptAudioMessage();
+    public String adaptImageMessage();
+
+
+
+}
