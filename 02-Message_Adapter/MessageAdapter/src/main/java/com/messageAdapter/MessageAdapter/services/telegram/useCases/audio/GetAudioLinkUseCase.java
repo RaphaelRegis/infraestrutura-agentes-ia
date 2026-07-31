0add40297eea73cbfb1abc284c639fd73a2a7589
RaphaelRegis <raph4life17@gmail.com>
2026-07-31 15:32:10 -0300

    chore: use cases para audio (sem implementação)

---
+++ 02-Message_Adapter/MessageAdapter/src/main/java/com/messageAdapter/MessageAdapter/services/telegram/useCases/audio/GetAudioLinkUseCase.java
@@ -0,0 +1,14 @@
+package com.messageAdapter.MessageAdapter.services.telegram.useCases.audio;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetAudioLinkUseCase {
+
+    public String getAudioLinkUseCase() {
+        return "";
+    }
+
+
+
+}
