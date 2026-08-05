8712abef9e661929e0ff41f7936ec5fda272c8b3
RaphaelRegis <raph4life17@gmail.com>
2026-08-05 18:35:43 -0300

    fix: cláusula throws para o método de transcrever áudio

--- 02-Message_Adapter/MessageAdapter/src/main/java/com/messageAdapter/MessageAdapter/services/telegram/TelegramAdapter.java
+++ 02-Message_Adapter/MessageAdapter/src/main/java/com/messageAdapter/MessageAdapter/services/telegram/TelegramAdapter.java
@@ -3,11 +3,13 @@ package com.messageAdapter.MessageAdapter.services.telegram;
 import com.messageAdapter.MessageAdapter.dto.telegram.audio.ReceivedTelegramAudioMessageDTO;
 import com.messageAdapter.MessageAdapter.dto.telegram.text.ReceivedTelegramTextMessageDTO;
 
+import java.io.IOException;
+
 public interface TelegramAdapter {
 
     void adaptTextMessage(ReceivedTelegramTextMessageDTO textMessageDTO);
 
-    void adaptAudioMessage(ReceivedTelegramAudioMessageDTO audioMessageDTO);
+    void adaptAudioMessage(ReceivedTelegramAudioMessageDTO audioMessageDTO) throws IOException, InterruptedException;
 
     String adaptImageMessage();
 
