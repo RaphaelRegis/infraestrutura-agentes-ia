95d6f1232d2fff0f826e1bed1fad39312ba37192
RaphaelRegis <raph4life17@gmail.com>
2026-08-10 17:20:02 -0300

    refact: melhor definição dos usecases comuns

--- 01-Lambda/impl/telegram_api/TelegramApiWorkflow.py
+++ 01-Lambda/impl/telegram_api/TelegramApiWorkflow.py
@@ -3,9 +3,9 @@ from impl.telegram_api.use_cases.send_to_message_adapter_usecase import send_to_
 from impl.telegram_api.use_cases.get_telegram_message_data_usecase import get_telegram_message_data_usecase
 from impl.telegram_api.use_cases.prepare_telegram_message_adapter_payload_usecase import \
     prepare_telegram_message_adapter_payload_usecase
-from impl.whatsapp_api.use_cases.GetAgentDataUsecase import get_agent_data
-from impl.whatsapp_api.use_cases.IsConversationPausedUsecase import is_conversation_paused
-from impl.whatsapp_api.use_cases.UpsertConversationUsecase import find_or_create_conversation_usecase
+from impl.common.use_cases.get_agent_data_usecase import get_agent_data_usecase
+from impl.common.use_cases.is_conversation_paused_usecase import is_conversation_paused_usecase
+from impl.common.use_cases.find_or_create_conversation_usecase import find_or_create_conversation_usecase
 from strategy import ProcessWorkflow
 
 class TelegramApiWorkflow(ProcessWorkflow.ProcessWorkflow):
@@ -22,13 +22,13 @@ class TelegramApiWorkflow(ProcessWorkflow.ProcessWorkflow):
         database_info = get_database_info_usecase("TELEGRAM_API")
 
         # pega os dados do agente
-        agent_data = get_agent_data(f"TELEGRAM_API_{message_data["agent_id"]}")
+        agent_data = get_agent_data_usecase(f"TELEGRAM_API_{message_data["agent_id"]}")
 
         # cria ou busca/atualiza a conversa
         ai_conversation = find_or_create_conversation_usecase(database_info)
 
         # verifica se esta pausada
-        is_paused = is_conversation_paused(ai_conversation)
+        is_paused = is_conversation_paused_usecase(ai_conversation)
 
         # prepara o corpo para o message_adapter
         message_adapter_payload = prepare_telegram_message_adapter_payload_usecase(message_data, ai_conversation, agent_data, is_paused)
