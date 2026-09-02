77fec9a8dd8c295f141fa9105d13b084c3598476
RaphaelRegis <raph4life17@gmail.com>
2026-09-02 18:56:32 -0300

    chore: casos de uso básicos do sistema

---
+++ 04-AISpringBootModel/AISpringBootModel/src/main/java/com/AISpringBootModel/AISpringBootModel/services/common/useCases/AnswerWithAIUsecase.java
@@ -0,0 +1,16 @@
+package com.AISpringBootModel.AISpringBootModel.services.common.useCases;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class AnswerWithAIUsecase {
+
+    public String answerWitAIUsecase() {
+        return "MOCK: resposta da IA";
+    }
+
+
+
+}
