f76642d222cdc5377225432a9ea3a92508defe87
RaphaelRegis <raph4life17@gmail.com>
2026-09-07 16:49:36 -0300

    refact: package das tools movido para diretório mais apropriado

--- 04-AISpringBootModel/AISpringBootModel/src/main/java/com/AISpringBootModel/AISpringBootModel/services/tools/telegram/AITelegramTools.java
+++ 04-AISpringBootModel/AISpringBootModel/src/main/java/com/AISpringBootModel/AISpringBootModel/services/tools/telegram/AITelegramTools.java
@@ -1,11 +1,11 @@
-package com.AISpringBootModel.AISpringBootModel.services.telegram.AITools;
+package com.AISpringBootModel.AISpringBootModel.services.tools.telegram;
 
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AgentTools {
+public class AITelegramTools {
 
     @Tool(description = "Essa tool existe apenas para fins de teste")
     public String exemploTool(@ToolParam(description = "Esse é apenas um parâmetro de exemplo") String parametro) {
