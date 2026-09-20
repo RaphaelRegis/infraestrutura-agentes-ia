9ea90ba7b14ddba5e3a070619aa6bed9239cbd11
RaphaelRegis <raph4life17@gmail.com>
2026-09-20 16:49:56 -0300

    fix: nome correto de campo do payload do Telegram

--- 03-Debouncer/debouncer/src/main/java/com/agents/debouncer/services/telegramDebouncer/TelegramDebounceServiceImpl.java
+++ 03-Debouncer/debouncer/src/main/java/com/agents/debouncer/services/telegramDebouncer/TelegramDebounceServiceImpl.java
@@ -23,7 +23,7 @@ public class TelegramDebounceServiceImpl implements TelegramDebouncerService {
     @Override
     public Mono<Void> debounceMessages(ReceivedTelegramMessageDTO messageDTO) {
 
-        String keyId = getKeyIdUseCase.getKeyIdUseCase(messageDTO.conversationID());
+        String keyId = getKeyIdUseCase.getKeyIdUseCase(messageDTO.chatID());
 
         return searchRedisMessageUseCase.searchRedisMessageUseCase(keyId)
                 .map(oldMessageString -> consolidateMessagesUseCase.consolidateMessagesUseCase(oldMessageString, messageDTO.message()))
@@ -36,7 +36,7 @@ public class TelegramDebounceServiceImpl implements TelegramDebouncerService {
                                         SendingTelegramMessageDTO sendingMessageDTO = new SendingTelegramMessageDTO(
                                                 messageDTO.agentID(),
                                                 messageDTO.contactName(),
-                                                messageDTO.conversationID(),
+                                                messageDTO.chatID(),
                                                 consolidatedMessage
                                         );
 
